54335e2c0170b538db1822f6bc0d5d331db8e5af
Syahludin <Guswansyahludin@gmail.com>
2026-08-25 00:36:25 +0700

    chore: remove temporary obsolete detail script

--- scripts/remove_obsolete_to_detail.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-p=Path('SiSi_Mobile/lib/screens/teknik_to_screen.dart')
-s=p.read_text()
-start=s.index('  Future<void> _detail(Map<String, dynamic> row) async {')
-end=s.index('\n}\n\nclass _SyncBadge', start)
-s=s[:start]+s[end:]
-start=s.index('\nclass _ToDetailSheet extends StatefulWidget')
-s=s[:start].rstrip()+'\n'
-p.write_text(s)
