b50f0cef4ef0f6a80a5fce755380110b21e0f891
guswan.syahludin <guswansyahludin@gmail.com>
2026-08-25 20:15:01 +0700

    feat: update workmanager dependencies and add platform support

--- SiSi_Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ SiSi_Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import geolocator_apple
 import shared_preferences_foundation
 import sqlite3_flutter_libs
 import url_launcher_macos
+import workmanager_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WorkmanagerPlugin.register(with: registry.registrar(forPlugin: "WorkmanagerPlugin"))
 }
