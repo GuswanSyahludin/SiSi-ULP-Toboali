1015512990cef4c4da95e697d24fc4c77ede2394
guswan.syahludin <guswansyahludin@gmail.com>
2026-08-27 16:00:15 +0700

    Fix duration calculation in getJadwalPadamWaText function for accurate reporting

--- SiSi_BackEnd/Teknik/Jadwal-Padam-Code.js
+++ SiSi_BackEnd/Teknik/Jadwal-Padam-Code.js
@@ -590,9 +590,7 @@ function getJadwalPadamWaText(params) {
         "*Jenis Pekerjaan:* " + _jpText_(x.jenis),
         "*Jam Padam:* " + _jpText_(x.jamPadam) + "WIB",
         "*Jam Nyala:* " + _jpText_(x.jamNyala) + "WIB",
-        "*Durasi:* " +
-          _jpWaNumber_((Number(_x_.durasi) || 0) * 60, 0) +
-          " Menit",
+        "*Durasi:* " + _jpWaNumber_((Number(x.durasi) || 0) * 60, 0) + " Menit",
         "*Daerah Padam:* " + _jpText_(x.daerah),
         "*Jumlah Gardu:* " + _jpWaNumber_(x.jumlahGardu, 0) + "Unit",
         "*Jumlah Pelanggan:* " +
