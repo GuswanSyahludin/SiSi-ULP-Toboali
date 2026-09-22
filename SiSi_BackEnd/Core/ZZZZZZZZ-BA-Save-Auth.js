b07a255c219dfd5e92308ad59cadb950a1a7ca8b
SyahludinGuswan <syahludin.guswan08@gmail.com>
2026-09-22 09:23:08 +0700

    security: require session for BA creation (#1)
    Merge authenticated BA Gardu creation boundary and regression coverage.

---
+++ SiSi_BackEnd/Core/ZZZZZZZZ-BA-Save-Auth.js
@@ -0,0 +1,21 @@
+/* SISI-REAUDIT-001/004: authenticated boundary for BA Gardu creation. */
+(function(){
+  var originalSave=simpanBeritaAcaraGardu;
+  var originalPage=getPageContent;
+
+  getPageContent=function(token,pageName){
+    var result=originalPage.apply(this,arguments);
+    if(result&&result.success&&pageName==='SIE-BeritaAcara'){
+      result.html+='<script>(function(g){var l=g.SISI_BUTUH_TOKEN;if(!Array.isArray(l)){l=[];g.SISI_BUTUH_TOKEN=l;}if(l.indexOf("simpanBeritaAcaraGardu")<0)l.push("simpanBeritaAcaraGardu");})(window);<\/script>';
+    }
+    return result;
+  };
+
+  simpanBeritaAcaraGardu=function(payload){
+    guard_(arguments,{ulp:true,aksi:'BA_GARDU_CREATE'});
+    payload=payload||{};
+    var secured={};
+    Object.keys(payload).forEach(function(key){if(key!=='token') secured[key]=payload[key];});
+    return originalSave.call(this,secured);
+  };
+})();
