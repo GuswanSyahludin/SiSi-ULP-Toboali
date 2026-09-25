a28db77bb1e56b47b90fc5d3e7da24e4af04fbbb
Guswan Syahludin <guswansyahludin745@gmail.com>
2026-09-25 13:13:54 +0700

    Initial commit

--- SiSi_Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ SiSi_Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import flutter_secure_storage_macos
 import geolocator_apple
 import shared_preferences_foundation
 import sqlite3_flutter_libs
@@ -14,6 +15,7 @@ import workmanager_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
